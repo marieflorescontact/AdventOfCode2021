dcc76c8bd4676e190721afc89bd70ddec93db20f
Marie Flores <marieflores.contact@gmail.com>
2022-01-05 11:17:56 +0100

    day1

---
+++ days/day1.py
@@ -0,0 +1,19 @@
+def main():
+    path = '../inputs/day1.txt'
+    file = open(path, 'r')
+    depths = []
+    for line in file:
+        depths.append(int(line))
+
+    increase = 0
+    sum = []
+    for i in range(len(depths) - 2):
+        sum_of_3 = depths[i] + depths[i + 1] + depths[i + 2]
+        sum.append(sum_of_3)
+    print(sum)
+    for j in range(len(sum) - 1):
+        if sum[j + 1] - sum[j] > 0:
+            increase += 1
+    print(increase)
+
+    file.close()
