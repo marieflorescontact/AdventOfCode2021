622ce9c108d5171235e2de9e9a5f32a1b0c3aa35
Marie Flores <marieflores.contact@gmail.com>
2022-01-05 12:28:34 +0100

    day2_part2

--- day2/day2.py
+++ day2/day2.py
@@ -1,22 +1,24 @@
 def main():
-    # forward x => horizontal += x
-    # down x => depth += x
-    # up x => deph -= x
+    # forward x => horizontal += x and depth += aim * x
+    # down x => aim += x
+    # up x => aim -= x
 
     path = 'day2.txt'
     file = open(path, 'r')
 
     horizontal = 0
     depth = 0
+    aim= 0
 
     for line in file:
         splited = line.split()
-        if splited[0] == 'forward':
-            horizontal += int(splited[1])
-        if splited[0] == 'up':
-            depth -= int(splited[1])
         if splited[0] == 'down':
-            depth += int(splited[1])
+            aim += int(splited[1])
+        if splited[0] == 'up':
+            aim -= int(splited[1])
+        if splited[0] == 'forward':
+            depth += int(splited[1]) * aim
+            horizontal += int(splited[1])
 
     mutliply_result = depth * horizontal
     print(mutliply_result)
